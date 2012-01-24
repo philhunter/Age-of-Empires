72afe72f796f97d64de4f9938ea6e03d3777524d
Carlos Soares <carlos.soares17@gmail.com>
2012-01-24 22:43:57 +0000

    Added Debuger Class for printing in console

---
+++ src/com/empires/Debuger/Debuger.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.empires.Debuger;
+
+/**
+ *
+ * @author karlos
+ */
+public class Debuger {
+    public void print(String debug){
+        System.out.println(debug);
+    }
+}
